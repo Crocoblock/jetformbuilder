ac0fef8b99fd8a9bced8003099938ab03d1d5134
girafffee <46720998+girafffee@users.noreply.github.com>
2021-06-04 09:19:18 +0300

    FIX: duplicating form

--- includes/form-actions/types/duplicate-action.php
+++ includes/form-actions/types/duplicate-action.php
@@ -28,10 +28,10 @@ class Duplicate_Action extends Base_Form_Action {
 	}
 
 	public function do_admin_action() {
-		$post_id      = $this->get_post_id_from_request();
-		$form_data[1] = $this->get_from_data( $post_id );
+		$post_id   = $this->get_post_id_from_request();
+		$form_data = $this->get_from_data( $post_id );
 
-		$this->import_form( $form_data );
+		$this->import_form( $form_data[1] );
 
 		wp_redirect( add_query_arg( array( 'post_type' => $this->post_type() ), admin_url( 'edit.php' ) ) );
 		die();

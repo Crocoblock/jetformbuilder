0f3a77d17cc4c54f87c08088564fb7439f612c04
girafffee <46720998+girafffee@users.noreply.github.com>
2021-06-11 14:50:59 +0300

    Improved `ActionFieldsMap` component

--- assets/src/package/components/action-fields-map.js
+++ assets/src/package/components/action-fields-map.js
@@ -3,10 +3,13 @@ const {
 	BaseControl,
 } = wp.components;
 
+
 export default function ActionFieldsMap( {
 									 fields = [],
 									 label = '[Empty label]',
-									 children = () => {}
+									 children = () => {},
+									 plainHelp = '',
+									customHelp = false
 								 } ) {
 
 	return <BaseControl
@@ -14,6 +17,12 @@ export default function ActionFieldsMap( {
 		key='jet_fields_map'
 	>
 		<div className='jet-user-fields-map__list'>
+			{ ( customHelp && 'function' === typeof customHelp ) && customHelp() }
+
+			{ Boolean( plainHelp.length ) && <span
+				className={ 'description-controls' }
+			>{ plainHelp }</span> }
+
 			{ fields.map( ( [ fieldId, fieldData ], index ) => <React.Fragment
 				key={ `field_in_map_${ fieldId + index }` }
 			>

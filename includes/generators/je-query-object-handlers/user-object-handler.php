3e38b3a32d0182b57da5fb5f7d8aeee3d038449d
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-07-11 11:49:26 +0300

    FIX: user query
    FIX: Crocoblock/issues-tracker#896

---
+++ includes/generators/je-query-object-handlers/user-object-handler.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Jet_Form_Builder\Generators\Je_Query_Object_Handlers;
+
+
+class User_Object_Handler extends Base_Object_Handler {
+
+	public function is_supported( $object ): bool {
+		return ( $object instanceof \WP_User );
+	}
+
+	protected function get_converted( $object ) {
+		return parent::get_converted( $object->data );
+	}
+
+	protected function get_label( $converted, $object ) {
+		return $object->{$this->label_field};
+	}
+
+}

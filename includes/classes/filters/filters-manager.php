daa11da659803878919682f5b69a29d596b5ac1b
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-08-05 12:28:15 +0300

    move `jet-form-builder/content-filters/args` filter
    Crocoblock/issues-tracker#1194

--- includes/classes/filters/filters-manager.php
+++ includes/classes/filters/filters-manager.php
@@ -4,6 +4,7 @@
 namespace Jet_Form_Builder\Classes\Filters;
 
 
+use Jet_Form_Builder\Actions\Types\Send_Email;
 use Jet_Form_Builder\Classes\Instance_Trait;
 use Jet_Form_Builder\Classes\Repository\Repository_Pattern_Trait;
 use Jet_Form_Builder\Exceptions\Action_Exception;
@@ -71,13 +72,10 @@ class Filters_Manager {
 			return $value;
 		}
 
-		$filter_arg = apply_filters(
-			'jet-form-builder/content-filters/args',
-			isset( $filter_data[2] ) ? trim( $filter_data[2], '()' ) : false,
-			$filter
-		);
+		$filter_arg = isset( $filter_data[2] ) ? trim( $filter_data[2], '()' ) : false;
 
 		return $filter->apply( $value, $filter_arg );
 	}
+
 
 }

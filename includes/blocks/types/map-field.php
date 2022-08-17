1cd4da044c674202b30306c3cd8fc6c9461b3da0
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-08-17 15:24:14 +0300

    Update map-field.php

--- includes/blocks/types/map-field.php
+++ includes/blocks/types/map-field.php
@@ -5,6 +5,13 @@ namespace Jet_Form_Builder\Blocks\Types;
 
 use Jet_Form_Builder\Compatibility\Jet_Engine\Blocks\Map_Field as Base_Map_Field;
 
+/**
+ * For JetEngine (3.0.3) compatibility
+ * @since 2.1.4
+ *
+ * Class Map_Field
+ * @package Jet_Form_Builder\Blocks\Types
+ */
 class Map_Field extends Base_Map_Field {
 
 }

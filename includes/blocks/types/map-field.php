f672bb42c67eadfa3d27a9cf95cf35ab0e8befc6
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-08-17 15:13:26 +0300

    FIX: Crocoblock/issues-tracker#1270

---
+++ includes/blocks/types/map-field.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Jet_Form_Builder\Blocks\Types;
+
+use Jet_Form_Builder\Compatibility\Jet_Engine\Blocks\Map_Field as Base_Map_Field;
+
+class Map_Field extends Base_Map_Field {
+
+}

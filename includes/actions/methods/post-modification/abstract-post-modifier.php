7aee97a403a28eb32e3f8ceb0d9df680f154103b
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-09-01 10:40:34 +0300

    FIX: Crocoblock/issues-tracker#1370

--- includes/actions/methods/post-modification/abstract-post-modifier.php
+++ includes/actions/methods/post-modification/abstract-post-modifier.php
@@ -14,8 +14,16 @@ abstract class Abstract_Post_Modifier extends Abstract_Modifier {
 	abstract public function get_id(): string;
 
 	public function before_run( Insert_Post $action ) {
-		$fields_map = $action->settings['fields_map'] ?? array();
-		$request    = jet_fb_action_handler()->request_data;
+		$request      = jet_fb_action_handler()->request_data;
+		$fields_map   = $action->settings['fields_map'] ?? array();
+		$post_status  = $action->settings['post_status'] ?? '';
+		$default_meta = $action->settings['default_meta'] ?? array();
+
+		$this->set( 'post_status', $post_status );
+
+		/** @var Post_Meta_Property $meta */
+		$meta = $this->get( 'meta_input' );
+		$meta->set_meta( Post_Meta_Property::prepare_meta( $default_meta ) );
 
 		$this->set_fields_map( $fields_map );
 		$this->set_request( $request );

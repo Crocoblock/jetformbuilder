9fe0bf12c9356f5bc23c0ebbf1f6c5053ecec8ca
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-11-22 16:26:25 +0200

    Update action details

--- assets/src/editor/form-actions/register.user/details.js
+++ assets/src/editor/form-actions/register.user/details.js
@@ -1,14 +1,18 @@
 const {
 	      __,
       } = wp.i18n;
+const {
+	      ExternalLink,
+      } = wp.components;
 
 const href = 'https://jetformbuilder.com/features/register-user/';
 
 function RegisterUserDetails() {
 	return <>
 		<div data-title={ __( 'Tutorial:', 'jet-form-builder' ) }>
-			<a href={ href }>{ __( 'Register User action',
-				'jet-form-builder' ) }</a>
+			<ExternalLink href={ href }>
+				{ __( 'Register User action', 'jet-form-builder' ) }
+			</ExternalLink>
 		</div>
 	</>;
 }
--- assets/src/editor/form-actions/save.record/details.js
+++ assets/src/editor/form-actions/save.record/details.js
@@ -1,25 +1,18 @@
 const {
 	      __,
       } = wp.i18n;
+const {
+	      ExternalLink,
+      } = wp.components;
 
 const href = 'https://jetformbuilder.com/features/wordpress-custom-form-records-page/';
 
 function SaveRecordDetails() {
 	return <>
-		{/*<div data-title={ __( 'Info:', 'jet-form-builder' ) }>
-			{ __(
-				'Action that stores the form entries in the database',
-				'jet-form-builder',
-			) }
-			<br/>
-			{ __(
-				'and makes them easily accessible from the dashboard.',
-				'jet-form-builder',
-			) }
-		</div>*/}
 		<div data-title={ __( 'Tutorial:', 'jet-form-builder' ) }>
-			<a href={ href }>{ __( 'Form Records Page',
-				'jet-form-builder' ) }</a>
+			<ExternalLink href={ href }>
+				{ __( 'Form Records Page', 'jet-form-builder' ) }
+			</ExternalLink>
 		</div>
 	</>;
 }

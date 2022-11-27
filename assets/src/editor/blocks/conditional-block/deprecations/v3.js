e6ae7ffdde74dccdccb32321edf3c8536c638dc0
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-11-27 12:31:20 +0200

    UPD: add migration for set value from conditional block

--- assets/src/editor/blocks/conditional-block/deprecations/v3.js
+++ assets/src/editor/blocks/conditional-block/deprecations/v3.js
@@ -1,6 +1,52 @@
 import metadata from '@blocks/conditional-block/block.json';
 import ConditionalSave from '../save';
 
+const {
+	      dispatch,
+      }         = wp.data;
+const { Tools } = JetFBActions;
+
+function migrateSetValue( block, conditions ) {
+	if (
+		!block.name.includes( 'jet-forms/' ) ||
+		!block.attributes.hasOwnProperty( 'value' )
+	) {
+		return;
+	}
+
+	const { updateBlock } = dispatch( 'core/block-editor' );
+
+	const groups = [];
+
+	for ( const {
+		            field,
+		            operator,
+		            set_value,
+		            value
+	            } of conditions
+		) {
+
+		const group = {
+			id: Tools.getRandomID(),
+			conditions: [
+				{ field, operator, value },
+			],
+			to_set: set_value ?? '',
+		};
+		groups.push( group );
+	}
+
+	setTimeout( () => {
+		updateBlock(
+			block.clientId, {
+				attributes: {
+					value: { groups },
+				},
+			},
+		);
+	} );
+}
+
 export default {
 	attributes: metadata.attributes,
 	supports: metadata.supports,
@@ -12,17 +58,25 @@ export default {
 			false === func_type && conditions?.length
 		);
 	},
-	migrate( attributes ) {
-		let parsed = {};
+	migrate( attributes, innerBlocks ) {
+		let parsed    = {},
+		    func_type = null;
+
+		const [ block ]          = innerBlocks || [];
+		const setValueConditions = [];
 
 		const conditions = attributes.conditions.sort(
 			current => 'show' === current.type ? -1 : 1,
 		);
 
-		let func_type = null;
-
 		for ( const condition of conditions ) {
 			condition.type = condition.type ?? 'show';
+
+			if ( 'set_value' === condition.type ) {
+				setValueConditions.push( condition );
+
+				continue;
+			}
 
 			if ( ![ 'show', 'hide' ].includes( condition.type ) ) {
 				continue;
@@ -39,6 +93,10 @@ export default {
 			}
 
 			parsed[ condition.field ] = condition;
+		}
+
+		if ( setValueConditions.length && 'object' === typeof block ) {
+			migrateSetValue( block, setValueConditions );
 		}
 
 		func_type = func_type ?? 'show';

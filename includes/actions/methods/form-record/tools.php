f72ca757e01e67c87679a705ba70a3d71b5b315d
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2022-12-29 11:40:25 +0200

    Added Jet_Form_Builder\Actions\Methods\Form_Record\Tools::update_record

--- includes/actions/methods/form-record/tools.php
+++ includes/actions/methods/form-record/tools.php
@@ -6,6 +6,8 @@ namespace Jet_Form_Builder\Actions\Methods\Form_Record;
 
 use Jet_Form_Builder\Actions\Methods\Form_Record\Query_Views\Record_Fields_View;
 use Jet_Form_Builder\Classes\Tools as GlobalTools;
+use Jet_Form_Builder\Db_Queries\Exceptions\Sql_Exception;
+use Jet_Form_Builder\Dev_Mode\Manager;
 
 class Tools {
 
@@ -15,6 +17,9 @@ class Tools {
 		self::parse_values( $request );
 
 		$values = wp_list_pluck( $request, 'field_value', 'field_name' );
+
+		// apply actions
+		jet_fb_action_handler()->set_form_id( $record['form_id'] ?? 0 );
 
 		jet_fb_action_handler()->add_request( $values );
 		jet_fb_request_handler()->set_request_type(
@@ -38,6 +43,22 @@ class Tools {
 
 			$field['field_value'] = GlobalTools::decode_json( $field['field_value'] );
 		}
+	}
+
+	/**
+	 * @param int $record_id
+	 *
+	 * @throws Sql_Exception
+	 */
+	public static function update_record( int $record_id ) {
+		if ( ! $record_id ) {
+			return;
+		}
+		$controller = ( new Controller() )->set_record_id( $record_id );
+		$controller->set_setting( 'save_errors', Manager::instance()->active() );
+
+		$controller->save_actions();
+		$controller->save_errors();
 	}
 
 }

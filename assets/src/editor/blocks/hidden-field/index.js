08ae30f5618e1430a0ec7f94b9a1de2a576d45a4
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-02-01 11:39:04 +0200

    FIX: converting text-field to hidden-field
    Crocoblock/suggestions#6408

--- assets/src/editor/blocks/hidden-field/index.js
+++ assets/src/editor/blocks/hidden-field/index.js
@@ -1,5 +1,5 @@
-import HeadingEdit from "./edit";
-import metadata from "@blocks/hidden-field/block.json";
+import HeadingEdit from './edit';
+import metadata from '@blocks/hidden-field/block.json';
 
 const { __ } = wp.i18n;
 
@@ -20,8 +20,8 @@ const settings = {
 	useEditProps: [ 'uniqKey', 'blockName' ],
 	example: {
 		attributes: {
-			field_value: 'post_id'
-		}
+			field_value: 'post_id',
+		},
 	},
 	transforms: {
 		to: [
@@ -29,7 +29,8 @@ const settings = {
 				type: 'block',
 				blocks: [ 'jet-forms/text-field' ],
 				transform: ( attributes ) => {
-					return createBlock( 'jet-forms/text-field', { ...attributes } );
+					return createBlock( 'jet-forms/text-field',
+						{ ...attributes } );
 				},
 				priority: 0,
 			},
@@ -41,6 +42,15 @@ const settings = {
 					'jet-forms/text-field',
 				],
 				transform: ( attributes ) => {
+					/**
+					 * @see https://github.com/Crocoblock/suggestions/issues/6408#issuecomment-1410778268
+					 * @since 3.0.0
+					 */
+					if ( attributes.default?.length ||
+						Object.keys( attributes.value )?.length
+					) {
+						attributes.field_value = '';
+					}
 					return createBlock( name, { ...attributes } );
 				},
 				priority: 0,
@@ -52,5 +62,5 @@ const settings = {
 export {
 	metadata,
 	name,
-	settings
+	settings,
 };

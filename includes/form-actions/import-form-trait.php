1c4da27da174846af82b56aa3425f6a318f9da3f
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-03-06 12:41:09 +0200

    FIX: Crocoblock/jetformbuilder#240
    FIX: Crocoblock/issues-tracker#2489

--- includes/form-actions/import-form-trait.php
+++ includes/form-actions/import-form-trait.php
@@ -8,7 +8,8 @@ trait Import_Form_Trait {
 	/**
 	 * Import form by data
 	 *
-	 * @param  array $form_data [description]
+	 * @param array $form_data [description]
+	 *
 	 * @return [type]            [description]
 	 */
 	public function import_form( $form_data = array() ) {
@@ -20,6 +21,10 @@ trait Import_Form_Trait {
 				'post_status' => 'publish',
 			)
 		);
+
+		if ( isset( $form_data['meta_input'] ) && is_array( $form_data['meta_input'] ) ) {
+			$form_data['meta_input'] = wp_slash( $form_data['meta_input'] );
+		}
 
 		$post_id = wp_insert_post(
 			array_merge(

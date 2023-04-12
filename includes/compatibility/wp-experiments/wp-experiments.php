05b3bc30b4495283cc266a4b989d5b5643d68904
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-04-12 20:56:38 +0300

    Add: jetStyle support for blocks

--- includes/compatibility/wp-experiments/wp-experiments.php
+++ includes/compatibility/wp-experiments/wp-experiments.php
@@ -12,12 +12,22 @@ if ( ! defined( 'WPINC' ) ) {
 
 class Wp_Experiments {
 
+	const STYLE_SUPPORT = 'jetStyle';
+
 	public function __construct() {
 		add_filter(
 			'render_block_jet-forms/choices-field',
 			array( $this, 'render_choice_with_layout' ),
 			10,
 			2
+		);
+
+		\WP_Block_Supports::get_instance()->register(
+			'jetStyle',
+			array(
+				'register_attribute' => array( $this, 'register_jet_style_support' ),
+				'apply'              => array( $this, 'apply_jet_style_support' ),
+			)
 		);
 	}
 
@@ -38,6 +48,52 @@ class Wp_Experiments {
 		);
 
 		return implode( "\r\n", $template );
+	}
+
+	public function register_jet_style_support( \WP_Block_Type $block_type ) {
+		// Setup attributes and styles within that if needed.
+		if ( ! $block_type->attributes ) {
+			$block_type->attributes = array();
+		}
+
+		if ( block_has_support( $block_type, array( self::STYLE_SUPPORT ) ) &&
+			! array_key_exists( 'style', $block_type->attributes )
+		) {
+			$block_type->attributes['style'] = array(
+				'type' => 'object',
+			);
+		}
+	}
+
+	public function apply_jet_style_support( \WP_Block_Type $block_type, array $block_attributes ): array {
+		$support_config = _wp_array_get( $block_type->supports, array( self::STYLE_SUPPORT ), false );
+		$root_styles    = $block_attributes['style'] ?? array();
+
+		if (
+			! is_array( $support_config ) ||
+			empty( $root_styles )
+		) {
+			return array();
+		}
+
+		$declarations = new \WP_Style_Engine_CSS_Declarations();
+
+		foreach ( $support_config as $css_var => $path_items ) {
+			foreach ( $path_items as $path_item ) {
+				if ( ! isset( \WP_Style_Engine::BLOCK_STYLE_DEFINITIONS_METADATA[ $path_item ] ) ) {
+					continue;
+				}
+
+				$declarations->add_declaration(
+					$css_var,
+					_wp_array_get( $root_styles, $path_items, '' )
+				);
+			}
+		}
+
+		return array(
+			'style' => $declarations->get_declarations_string(),
+		);
 	}
 
 }

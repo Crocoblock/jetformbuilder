932935bf905c9dbbd76bffac076f280752669d98
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-04-18 10:35:05 +0300

    FIX: Crocoblock/issues-tracker#2744

--- assets/src/package/blocks/helpers/blocksRecursiveIterator.js
+++ assets/src/package/blocks/helpers/blocksRecursiveIterator.js
@@ -5,7 +5,7 @@ function blocksRecursiveIterator( blockParserFunc ) {
 
 		blocks = blocks || select( 'core/block-editor' ).getBlocks();
 
-		blocks.map( block => {
+		blocks.forEach( block => {
 			blockParserFunc( block, parent );
 
 			if ( block.innerBlocks.length ) {
@@ -17,7 +17,49 @@ function blocksRecursiveIterator( blockParserFunc ) {
 					block.innerBlocks,
 					currentParent,
 				);
+
+				return;
 			}
+
+			if ( 'core/block' !== block.name ) {
+				return;
+			}
+
+			/**
+			 * Hardcode the receipt of internal blocks "Reusable block".
+			 * This is the only way so far.
+			 *
+			 * Added after major refactoring
+			 * @since 3.0.4
+			 *
+			 * Introduced
+			 * @since 2.1.1
+			 *
+			 * @type {{clientId: String, innerBlocks: Array}[]}
+			 */
+			let innerReusable = select( 'core/block-editor' )?.
+				__unstableGetClientIdsTree?.(
+					block.clientId,
+				);
+
+			if ( ! innerReusable?.length ) {
+				return;
+			}
+
+			const innerReusableIds = innerReusable.map(
+				( { clientId } ) => clientId,
+			);
+
+			/**
+			 * We update this variable because `__unstableGetClientIdsTree`
+			 * returns insufficient information about the block.
+			 * Namely, the `name` property is missing
+			 */
+			innerReusable = select( 'core/block-editor' ).getBlocksByClientId(
+				innerReusableIds,
+			);
+
+			blocksRecursiveIterator( innerReusable );
 		} );
 	};
 

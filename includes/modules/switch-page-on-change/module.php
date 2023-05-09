81159c1f822deac1185de3f8b23fcc905982c87b
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-05-09 18:34:08 +0300

    Add initial switch-page-on-change module

--- includes/modules/switch-page-on-change/module.php
+++ includes/modules/switch-page-on-change/module.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Jet_Form_Builder\Modules\Jet_Style;
+namespace Jet_Form_Builder\Modules\Switch_Page_On_Change;
 
 use Jet_Form_Builder\Classes\Arrayable\Array_Tools;
 use Jet_Form_Builder\Modules\Base_Module\Base_Module_After_Install_It;
@@ -16,40 +16,23 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * @since 3.1.0
- *
- * Class Module
- * @package Jet_Form_Builder\Modules\Jet_Style
- */
-class Module implements
-	Base_Module_It,
-	Base_Module_Handle_It,
-	Base_Module_Url_It,
-	Base_Module_After_Install_It {
+class Module implements Base_Module_It, Base_Module_Url_It, Base_Module_Handle_It, Base_Module_After_Install_It {
 
 	use Base_Module_Url_Trait;
 	use Base_Module_Handle_Trait;
 
-	const SUPPORT_NAME = 'jetStyle';
-
-	/**
-	 * @var Css_Compiler_Manager
-	 */
-	private $compiler;
+	const SUPPORT_NAME   = 'jetFBSwitchPageOnChange';
+	const ATTRIBUTE_NAME = 'switch_on_change';
 
 	public function rep_item_id() {
-		return 'jet-style';
+		return 'switch-page-on-change';
 	}
 
 	public function condition(): bool {
-		// since WP 6.2
-		return class_exists( '\WP_HTML_Tag_Processor' );
+		return true;
 	}
 
 	public function on_install() {
-		$this->compiler = new Css_Compiler_Manager();
-
 		\WP_Block_Supports::get_instance()->register(
 			self::SUPPORT_NAME,
 			array(
@@ -60,29 +43,9 @@ class Module implements
 	}
 
 	public function init_hooks() {
-		add_action(
-			'jet-form-builder/editor-assets/before',
-			array( $this, 'register_scripts' ),
-			0
-		);
 	}
 
 	public function remove_hooks() {
-		remove_action(
-			'jet-form-builder/editor-assets/before',
-			array( $this, 'register_scripts' ),
-			0
-		);
-	}
-
-	public function register_scripts() {
-		wp_enqueue_script(
-			$this->get_handle(),
-			$this->get_url( 'assets-build/js/editor/main{min}.js' ),
-			array(),
-			jet_form_builder()->get_version(),
-			true
-		);
 	}
 
 	public function register_support( \WP_Block_Type $block_type ) {
@@ -92,29 +55,27 @@ class Module implements
 		}
 
 		if ( block_has_support( $block_type, array( self::SUPPORT_NAME ) ) &&
-			! array_key_exists( 'style', $block_type->attributes )
+			! array_key_exists( self::ATTRIBUTE_NAME, $block_type->attributes )
 		) {
-			$block_type->attributes['style'] = array(
-				'type' => 'object',
+			$block_type->attributes[ self::ATTRIBUTE_NAME ] = array(
+				'type'    => 'boolean',
+				'default' => false,
 			);
 		}
 	}
 
 	public function apply_support( \WP_Block_Type $block_type, array $block_attributes ): array {
 		$support_config = Array_Tools::get( $block_type->supports, array( self::SUPPORT_NAME ), false );
-		$root_styles    = $block_attributes['style'] ?? array();
+		$value          = $block_attributes[ self::ATTRIBUTE_NAME ] ?? array();
 
 		if (
 			! is_array( $support_config ) ||
-			empty( $root_styles )
+			empty( $value )
 		) {
 			return array();
 		}
+		// todo
 
-		return $this->get_compiler()->compile( $root_styles, $support_config );
-	}
-
-	public function get_compiler(): Css_Compiler_Manager {
-		return $this->compiler;
+		return array();
 	}
 }

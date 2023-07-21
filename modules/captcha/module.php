39f8624647024a30488a0eb9d72fed625ca25b41
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-07-21 16:38:22 +0300

    FIX: phpcbf & update lint workflow

--- modules/captcha/module.php
+++ modules/captcha/module.php
@@ -337,10 +337,10 @@ final class Module implements
 
 		$current->set_exist_container(
 			! empty(
-			Block_Helper::find_by_block_name(
-				$blocks,
-				'jet-forms/captcha-container'
-			)
+				Block_Helper::find_by_block_name(
+					$blocks,
+					'jet-forms/captcha-container'
+				)
 			)
 		);
 

6c664ba0d4408ae396124a42959972200217de8e
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-08-18 17:35:52 +0300

    ADD: check validity of json source for options
    Crocoblock/issues-tracker#2607

--- assets/src/editor/components/base-select-check-radio/from-manual/bulk-options.js
+++ assets/src/editor/components/base-select-check-radio/from-manual/bulk-options.js
@@ -1,5 +1,8 @@
+import ListActionItem from '../../../plugins/actions/action.item';
+
 const {
 	      __,
+	      sprintf,
       } = wp.i18n;
 
 const {
@@ -10,6 +13,8 @@ const {
 const {
 	      useState,
 	      useEffect,
+	      Children,
+	      cloneElement,
       } = wp.element;
 
 let {
@@ -18,7 +23,6 @@ let {
 	    BaseControl,
 	    ButtonGroup,
 	    Button,
-	    Flex,
 	    __experimentalSpacer,
 	    Spacer,
     } = wp.components;
@@ -32,7 +36,12 @@ const {
       } = JetFBHooks;
 
 const {
+	      isEmpty,
+      } = JetFBActions;
+
+const {
 	      ActionModalFooterSlotFill,
+	      BaseHelp,
       } = JetFBComponents;
 
 const {
@@ -45,21 +54,101 @@ const baseBulk = window.JetFBBulkOptions.sources[
 
 const toBulk = val => JSON.stringify( val, undefined, 4 );
 
+const resolveErrors = bulk => {
+	let options;
+	let currentErrors = [];
+
+	try {
+		options = JSON.parse( bulk );
+	}
+	catch ( error ) {
+		return [ error.message ];
+	}
+
+	if ( !Array.isArray( options ) ) {
+		return [
+			__(
+				'Your value must be an array.',
+				'jet-form-builder',
+			),
+		];
+	}
+
+	for ( const [ index, current ] of Object.entries(
+		options,
+	) ) {
+		if ( 'object' !== typeof current ) {
+			currentErrors.push(
+				__(
+					`[row:${ index }] Each array element must be an object`,
+					'jet-form-builder',
+				),
+			);
+
+			continue;
+		}
+
+		const { label, value, ...other } = current;
+
+		if ( !isEmpty( label ) ||
+			!isEmpty( value ) ||
+			isEmpty( other )
+		) {
+			continue;
+		}
+
+		currentErrors.push(
+			__(
+				`[row:${ index }] You have an incorrect object format. 
+There are no values for the value or label keys`,
+				'jet-form-builder',
+			),
+		);
+	}
+
+	if ( 1 >= currentErrors.length ) {
+		return currentErrors;
+	}
+
+	const [ first, ...otherErrors ] = currentErrors;
+
+	return [
+		first,
+		sprintf(
+			__(
+				'...and %d more incorrect elements',
+				'jet-form-builder',
+			),
+			otherErrors.length,
+		),
+	];
+};
+
 function BulkOptions( { setModalContent } ) {
 	const [ bulk, setBulk ] = useState(
 		() => toBulk( baseBulk ),
 	);
 
 	const [ bulkSelect, setBulkSelect ] = useState( 'base' );
+	const [ errors, setErrors ]         = useState( [] );
 	const { attributes, setAttributes } = useScopedAttributesContext();
 
-	const onUploadBulkSource = () => {
-		setBulk( toBulk( window.JetFBBulkOptions.sources[ bulkSelect ] ) );
-	};
+	useEffect(
+		() => {
+			setBulk( toBulk( window.JetFBBulkOptions.sources[ bulkSelect ] ) );
+		},
+		[ bulkSelect ],
+	);
 
 	useEffect(
-		onUploadBulkSource,
-		[ bulkSelect ]
+		() => {
+			setErrors( resolveErrors( bulk ) );
+		},
+		[ bulk ],
+	);
+
+	const errorsElements = errors.map(
+		error => <li>{ error }</li>,
 	);
 
 	return <>
@@ -71,18 +160,40 @@ function BulkOptions( { setModalContent } ) {
 			/>
 		</BaseControl>
 		<hr style={ { marginBottom: '1.5em', marginTop: '1.5em' } }/>
+		<BaseHelp>
+			{ __(
+				'In this field we have to insert an array of objects in JSON format.',
+				'jet-form-builder',
+			) }
+		</BaseHelp>
 		<TextareaControl
 			className="jet-control-clear"
 			value={ bulk }
 			onChange={ setBulk }
-			rows={ 20 }
+			rows={ 18 }
 		/>
+		<BaseHelp style={ {
+			color: Boolean( errors.length )
+			       ? '#9e1313'
+			       : '#208949',
+		} }>
+			{ Boolean( errors.length )
+			  ? <ul className="jet-fb-ul-revert-layer">
+				  { Children.map(
+					  errorsElements,
+					  cloneElement,
+				  ) }
+			  </ul>
+			  : __( 'You have the correct value format', 'jet-form-builder' )
+			}
+		</BaseHelp>
 		<ModalFooterFill>
 			<ButtonGroup
 				className="jet-form-edit-modal__actions"
 			>
 				<Button
 					isPrimary
+					disabled={ Boolean( errors.length ) }
 					onClick={ () => {
 						setAttributes( {
 							field_options: [

4e6aa13924361d69b7ba131f95c599633f6d216b
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-09-14 09:47:19 +0300

    Added initial Bricks module compatibility

---
+++ compatibility/bricks/bricks.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace JFB_Compatibility\Bricks;
+
+use JFB_Components\Compatibility\Base_Compat_Handle_Trait;
+use JFB_Components\Compatibility\Base_Compat_Url_Trait;
+use JFB_Components\Module\Base_Module_Handle_It;
+use JFB_Components\Module\Base_Module_It;
+use JFB_Components\Module\Base_Module_Url_It;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class Bricks implements Base_Module_It, Base_Module_Handle_It, Base_Module_Url_It {
+
+	use Base_Compat_Handle_Trait;
+	use Base_Compat_Url_Trait;
+
+	public function rep_item_id() {
+		return 'bricks';
+	}
+
+	public function condition(): bool {
+		// todo correct check
+		return false;
+	}
+
+	public function init_hooks() {
+		// todo add hooks & filters
+	}
+
+	public function remove_hooks() {
+		// todo remove hooks & filters
+	}
+
+}

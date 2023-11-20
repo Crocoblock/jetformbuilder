7f032d078a686a0b8b493b8d10461d930c4e814b
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-11-20 17:59:49 +0200

    UPD: added primary key for `tokens_to_records` by perfomance reasons

--- modules/webhook/db/models/tokens-to-records-model.php
+++ modules/webhook/db/models/tokens-to-records-model.php
@@ -19,6 +19,7 @@ class Tokens_To_Records_Model extends Base_Db_Model {
 
 	public static function schema(): array {
 		return array(
+			'id'        => 'bigint(20) NOT NULL AUTO_INCREMENT',
 			'token_id'  => 'bigint(20) NOT NULL',
 			'record_id' => 'bigint(20) NOT NULL',
 		);
@@ -26,6 +27,7 @@ class Tokens_To_Records_Model extends Base_Db_Model {
 
 	public static function schema_keys(): array {
 		return array(
+			'id'        => 'primary key',
 			'token_id'  => 'index',
 			'record_id' => 'index',
 		);

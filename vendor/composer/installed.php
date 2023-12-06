480a1256a99ac5f38e55dc32b0005d7c7b48dab1
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-12-06 11:47:57 +0200

    Added vendor dir with woo/AS & composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'crocoblock/jetformbuilder',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c428a2358b23775fd34405b7cc2147c2be986ab',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'crocoblock/jetformbuilder' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c428a2358b23775fd34405b7cc2147c2be986ab',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.7.0',
+            'version' => '3.7.0.0',
+            'reference' => '7a2046a5e6a15786068bb0298407522f30c694b4',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

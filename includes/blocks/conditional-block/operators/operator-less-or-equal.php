a691dea289fed3b158cf90d7a78a80a9c5bf7ad5
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2024-01-18 10:34:05 +0200

    Resolve: #375

---
+++ includes/blocks/conditional-block/operators/operator-less-or-equal.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Jet_Form_Builder\Blocks\Conditional_Block\Operators;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class Operator_Less_Or_Equal extends Base_Operator {
+
+	const ID = 'less_or_eq';
+
+	public function get_id(): string {
+		return self::ID;
+	}
+
+	public function get_title(): string {
+		return __( 'Less than or equal', 'jet-form-builder' );
+	}
+
+	public function is_supported(): bool {
+		return false;
+	}
+
+}

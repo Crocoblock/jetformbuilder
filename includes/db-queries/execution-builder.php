f98798aea2e704634662194694a6a0f9a6442759
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2024-04-16 11:36:39 +0300

    UPD: logging database error which are related to the records_* tables Crocoblock/issues-tracker#2634

--- includes/db-queries/execution-builder.php
+++ includes/db-queries/execution-builder.php
@@ -89,19 +89,31 @@ class Execution_Builder {
 		// phpcs:ignore WordPress.DB.DirectDatabaseQuery
 		$wpdb->insert( $model::table(), $insert_columns, $format );
 
-		if ( ! $wpdb->rows_affected ) {
-			throw new Sql_Exception(
-				esc_html( "Something went wrong on insert into: {$model::table()}" ),
-				// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
-				$wpdb->last_error,
-				// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
-				$insert_columns
+		if ( $wpdb->rows_affected ) {
+			$model->after_insert( $insert_columns );
+
+			return $wpdb->insert_id;
+		}
+
+		if ( 0 === strpos( $model::table_name(), 'records' ) ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions
+			error_log(
+				sprintf(
+					'[jetformbuilder]: %s',
+					$wpdb->last_error
+				)
 			);
 		}
 
-		$model->after_insert( $insert_columns );
-
-		return $wpdb->insert_id;
+		throw new Sql_Exception(
+			esc_html(
+				"Something went wrong on insert into: {$model::DB_TABLE_PREFIX}{$model::table_name()}"
+			),
+			// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
+			$wpdb->last_error,
+			// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
+			$insert_columns
+		);
 	}
 
 	/**

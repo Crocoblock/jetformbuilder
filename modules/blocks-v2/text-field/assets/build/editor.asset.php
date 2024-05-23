b98f329f95810d03ab11ba39ec64b33f2a33c6bf
girafffee <46720998+girafffee@users.noreply.github.com>
2024-05-23 17:59:06 +0300

    UPD: improve style of eye-icon for password field

--- modules/blocks-v2/text-field/assets/build/editor.asset.php
+++ modules/blocks-v2/text-field/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'dc8dd3fc2d0273675fc7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'c0994ee47f0d1c12ad68');
--- modules/blocks-v2/text-field/assets/build/frontend/field.asset.php
+++ modules/blocks-v2/text-field/assets/build/frontend/field.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '9990b785f0ee99fb20b9');
+<?php return array('dependencies' => array(), 'version' => 'dd60dcd9727d901a46f2');

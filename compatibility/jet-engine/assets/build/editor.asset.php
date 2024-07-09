edce3ce56211d4892a1467f1940b5f50d9460a1e
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2024-07-09 09:31:01 +0300

    UPD: icon for update action Crocoblock/issues-tracker#5058

--- compatibility/jet-engine/assets/build/editor.asset.php
+++ compatibility/jet-engine/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-data', 'wp-i18n'), 'version' => '949b1fcae2ef5fe911c8');
+<?php return array('dependencies' => array('react', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '4f224a2b4f23c33607e3');
--- compatibility/jet-engine/assets/src/editor/update.options/index.js
+++ compatibility/jet-engine/assets/src/editor/update.options/index.js
@@ -1,9 +1,11 @@
 import UpdateOptionsRender from './edit';
 import { __ } from '@wordpress/i18n';
+import { cog } from '@wordpress/icons';
 
 export default {
 	type: 'update_options',
 	label: __( 'Update Options', 'jet-form-builder' ),
 	edit: UpdateOptionsRender,
 	category: 'content',
+	icon: cog,
 };

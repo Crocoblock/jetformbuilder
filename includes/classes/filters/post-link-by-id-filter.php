23cdb5569da7f3a2b9a2b90f2a6451fb09d187bf
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2024-07-17 09:39:21 +0300

    UPD: improve `post_link_by_id` filter for macros Crocoblock/issues-tracker#5520

--- includes/classes/filters/post-link-by-id-filter.php
+++ includes/classes/filters/post-link-by-id-filter.php
@@ -16,14 +16,24 @@ class Post_Link_By_Id_Filter extends Base_Multiple_Filter {
 		return 'post_link_by_id';
 	}
 
+	public function callback_args(): array {
+		return array( '' );
+	}
+
 	protected function apply_item( $item, ...$args ): string {
-		/** @var Send_Email_Action $action */
-		$action = jet_fb_action_handler()->get_current_action();
+		list( $format ) = $args;
+
+		if ( ! $format && jet_fb_action_handler()->in_loop() ) {
+			/** @var Send_Email_Action $action */
+			$action = jet_fb_action_handler()->get_current_action();
+
+			$format = ( $action instanceof Send_Email_Action ) && $action->is_html() ? 'html' : 'plain';
+		}
 
 		$url   = get_permalink( $item );
 		$title = get_the_title( $item );
 
-		if ( ! $action->is_html() ) {
+		if ( 'html' !== $format ) {
 			return sprintf( '%1$s: %2$s', $title, $url );
 		}
 

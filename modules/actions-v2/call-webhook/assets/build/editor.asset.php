f73d1f309c23b1bb9ac83913f44f2f16c35426b3
yurkodmtr <4au.4au.4au@gmail.com>
2024-10-07 12:18:06 +0300

    FIX: set min value for input https://github.com/Crocoblock/issues-tracker/issues/12236

--- modules/actions-v2/call-webhook/assets/build/editor.asset.php
+++ modules/actions-v2/call-webhook/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-primitives'), 'version' => 'b23f7734c31c646e6284');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-primitives'), 'version' => '5700d2bcc9b3dda30082');

fc93f17e6789b57c31845f338251da69d9746bc5
Gawuww <gawuww3@gmail.com>
2024-11-25 16:07:50 +0200

    FIX: composer files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit7b3df49a916ab03743bf74be22edb696
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit7b3df49a916ab03743bf74be22edb696::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }

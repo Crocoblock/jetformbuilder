59c5d906e824ddfc27a5dcf4c9b10869970003b7
Gawuww <gawuww3@gmail.com>
2024-11-25 15:38:47 +0200

    FIX: Google reCAPTCHA Score Threshold option description styles https://github.com/Crocoblock/issues-tracker/issues/12787

--- modules/captcha/assets/build/re-captcha-v3/editor.asset.php
+++ modules/captcha/assets/build/re-captcha-v3/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-i18n'), 'version' => '3ba678907c7fb39e7b91');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-i18n'), 'version' => '20c58aa47c9da906bb89');

87f0af2f918589a8755e08d1718a50eb3af84998
yurkodmtr <4au.4au.4au@gmail.com>
2025-02-19 17:48:34 +0200

    FIX: PHP Lint & Test / PHP CodeSniffer Linter https://github.com/Crocoblock/suggestions/issues/7769

--- modules/validation/ssr/is-field-value-unique.php
+++ modules/validation/ssr/is-field-value-unique.php
@@ -21,29 +21,37 @@ class Is_Field_Value_Unique extends Base_Validation_Callback {
 	}
 
 	public function is_valid( $value, Parser_Context $context ): bool {
-		$request = $context->get_request();
+		$request    = $context->get_request();
 		$field_name = $request['_jfb_validation_path'][0];
-		$form_id = $request['_jet_engine_booking_form_id'];
+		$form_id    = $request['_jet_engine_booking_form_id'];
 
 		global $wpdb;
-		$record_ids = $wpdb->get_col($wpdb->prepare(
-			"SELECT id FROM {$wpdb->prefix}jet_fb_records WHERE form_id = %d AND status = %s",
-			$form_id,
-			'success'
-		));
-		if (!empty($record_ids)) {
-			$exists = $wpdb->get_var($wpdb->prepare(
-				"SELECT COUNT(*) FROM {$wpdb->prefix}jet_fb_records_fields 
-         		WHERE record_id IN (" . implode(',', array_map('intval', $record_ids)) . ")
-         		AND field_name = %s
-         		AND field_value = %s",
-				$field_name,
-				$value
-			));
-			if ($exists) {
+		$record_ids = $wpdb->get_col(
+			$wpdb->prepare(
+				'SELECT id FROM ' . $wpdb->prefix . 'jet_fb_records WHERE form_id = %d AND status = %s',
+				$form_id,
+				'success'
+			)
+		);
+
+		if ( ! empty( $record_ids ) ) {
+			$placeholders = implode( ', ', array_fill( 0, count( $record_ids ), '%d' ) );
+
+			$exists = $wpdb->get_var(
+				$wpdb->prepare(
+					'SELECT COUNT(*) FROM ' . $wpdb->prefix . 'jet_fb_records_fields 
+					WHERE record_id IN (' . $placeholders . ') 
+					AND field_name = %s 
+					AND field_value = %s',
+					array_merge( $record_ids, [ $field_name, $value ] )
+				)
+			);
+
+			if ( $exists ) {
 				return false;
 			}
 		}
+
 		return true;
 	}
 }

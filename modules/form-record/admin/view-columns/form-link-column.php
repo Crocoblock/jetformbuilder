62f02c67b0b7379b198e275b56532ad18590ca67
yurkodmtr <4au.4au.4au@gmail.com>
2025-03-20 14:04:17 +0200

    FIX: Implement a mechanism to control visibility to jetFormBuilder -> Form Records for unprivileged users https://github.com/Crocoblock/issues-tracker/issues/15034

--- modules/form-record/admin/view-columns/form-link-column.php
+++ modules/form-record/admin/view-columns/form-link-column.php
@@ -31,7 +31,14 @@ class Form_Link_Column extends Column_Advanced_Base {
 	public function get_value( array $record = array() ) {
 		$form_id = parent::get_value( $record );
 		$form    = get_post( $form_id );
-
+		if ( !current_user_can( 'manage_options' ) ) {
+			return array(
+				'text'  => Post_Tools::get_title( $form ),
+				'href'  => '',
+				'title' => __( '', 'jet-form-builder' ),
+				'type'  => '',
+			);
+		}
 		return array(
 			'text'  => Post_Tools::get_title( $form ),
 			'href'  => get_edit_post_link( $form, false ),

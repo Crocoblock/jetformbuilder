6ea9cee1ffa7252334d534281bd6dc7383b14bdb
yurkodmtr <4au.4au.4au@gmail.com>
2025-03-28 11:43:40 +0200

    FIX: Implement a mechanism to control visibility to jetFormBuilder -> Form Records for unprivileged users https://github.com/Crocoblock/issues-tracker/issues/15034

--- modules/form-record/admin/view-columns/form-link-column.php
+++ modules/form-record/admin/view-columns/form-link-column.php
@@ -8,6 +8,7 @@ use Jet_Form_Builder\Admin\Pages\Pages_Manager;
 use Jet_Form_Builder\Admin\Table_Views\Column_Advanced_Base;
 use Jet_Form_Builder\Classes\Post\Post_Tools;
 use Jet_Form_Builder\Exceptions\Repository_Exception;
+use JFB_Modules\Post_Type\Module;
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -31,10 +32,23 @@ class Form_Link_Column extends Column_Advanced_Base {
 	public function get_value( array $record = array() ) {
 		$form_id = parent::get_value( $record );
 		$form    = get_post( $form_id );
+
 		if ( !current_user_can( 'manage_options' ) ) {
+			$href = null;
+			$capabilities = Module::CAPABILITIES;
+			$has_all_capabilities = true;
+			foreach ( $capabilities as $capability ) {
+				if ( ! current_user_can( $capability ) ) {
+					$has_all_capabilities = false;
+					break;
+				}
+			}
+			if ( $has_all_capabilities ) {
+				$href = admin_url("post.php?post={$form_id}&action=edit");
+			}
 			return array(
 				'text'  => Post_Tools::get_title( $form ),
-				'href'  => null,
+				'href'  => $href,
 				'title' => __( '', 'jet-form-builder' ),
 				'type'  => '',
 			);

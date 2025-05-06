1dedeb494390e638cbb294b3c9ee34d080dc2ae7
Gawuww <gawuww3@gmail.com>
2025-05-06 14:58:00 +0300

    FIX: build js

--- modules/option-field/assets/build/editor.asset.php
+++ modules/option-field/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a73e38f2f1f162c042dc');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'efc0345d234ac9800555');

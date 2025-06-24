863b81f7663bd038c1fd90392fdd93793a04e17a
Gawuww <gawuww3@gmail.com>
2025-06-24 10:43:02 +0300

    UPD: 'Insert/Update Term' action icons https://github.com/Crocoblock/issues-tracker/issues/3036

--- modules/actions-v2/insert-term/assets/build/editor.asset.php
+++ modules/actions-v2/insert-term/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f1cc99a302d0f19f78f6');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ddcc334dcaed44b68aee');

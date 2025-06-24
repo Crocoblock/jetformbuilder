c0729dca0c39df7438cd680a9c9374e1cf3513e6
Gawuww <gawuww3@gmail.com>
2025-06-24 10:20:42 +0300

    UPD: custom options icons https://github.com/Crocoblock/issues-tracker/issues/16371

--- modules/components/assets/build/index.asset.php
+++ modules/components/assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'f1f1fd4a2a62d9a97c6e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '41df189c357b1153f9c2');
--- modules/option-field/assets/build/editor.asset.php
+++ modules/option-field/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '867d68e15b1c65e93a5f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a3b1693ad58bb32fa2c7');

6b292023a25649d1746691801ed2ef6a843f4cd8
Gawuww <gawuww3@gmail.com>
2025-10-22 10:38:19 +0300

    FIX: Disable field validation on form init for Advanced Validation type https://github.com/Crocoblock/issues-tracker/issues/16433

--- assets/build/frontend/advanced.reporting.asset.php
+++ assets/build/frontend/advanced.reporting.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '6fc872528ddc539d2c40');
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '8ff3219219229b3acd2f');

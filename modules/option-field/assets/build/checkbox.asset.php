945a01a72fdb06c66517eba29b4c9ec6342186fb
yurkodmtr <4au.4au.4au@gmail.com>
2025-11-25 13:37:32 +0200

    FIX: Checkbox with comma in value cannot be selected individually in group https://github.com/Crocoblock/jetformbuilder/issues/569

--- modules/option-field/assets/build/checkbox.asset.php
+++ modules/option-field/assets/build/checkbox.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f54eb583054648e8615b');
+<?php return array('dependencies' => array(), 'version' => 'd2b60db213d98b8c8b0d');
--- modules/option-field/assets/build/editor.asset.php
+++ modules/option-field/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a3b1693ad58bb32fa2c7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5168e610fa37d3049e17');

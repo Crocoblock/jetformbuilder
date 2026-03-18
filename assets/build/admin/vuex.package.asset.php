78ccdf5917e0092b503c1879a2b767556e4e98d0
Gawuww <gawuww3@gmail.com>
2026-03-18 08:38:55 +0200

    FIX: Incorrect display of large numbers in Form Records https://github.com/Crocoblock/issues-tracker/issues/19295

--- assets/build/admin/vuex.package.asset.php
+++ assets/build/admin/vuex.package.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '22d5a02ad7e472fca093');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '65e888b947430720bfbe');

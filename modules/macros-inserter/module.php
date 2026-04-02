9c16a7c44eca63750603bac5e0369a1ef5fb5143
yurkodmtr <4au.4au.4au@gmail.com>
2026-04-02 12:43:40 +0300

    UPD: fix block registration https://github.com/Crocoblock/issues-tracker/issues/5654

--- modules/macros-inserter/module.php
+++ modules/macros-inserter/module.php
@@ -10,7 +10,7 @@ use JFB_Components\Module\Base_Module_It;
 use JFB_Components\Module\Base_Module_Url_It;
 use JFB_Components\Module\Base_Module_Url_Trait;
 
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
@@ -18,35 +18,56 @@ class Module implements
 	Base_Module_It,
 	Base_Module_Url_It,
 	Base_Module_Handle_It,
-	Base_Module_Dir_It {
+	Base_Module_Dir_It
+{
 
 	use Base_Module_Handle_Trait;
 	use Base_Module_Url_Trait;
 	use Base_Module_Dir_Trait;
 
-	public function rep_item_id() {
+	public function rep_item_id()
+	{
 		return 'macros-inserter';
 	}
 
-	public function condition(): bool {
+	public function condition(): bool
+	{
 		return true;
 	}
 
-	public function init_hooks() {
+	public function init_hooks()
+	{
+		add_action('init', array($this, 'register_block_type'));
+
 		add_action(
 			'jet-form-builder/editor-assets/before',
-			array( $this, 'register_editor_assets' )
+			array($this, 'register_editor_assets')
 		);
 	}
 
-	public function remove_hooks() {
+	public function remove_hooks()
+	{
+		remove_action('init', array($this, 'register_block_type'));
+
 		remove_action(
 			'jet-form-builder/editor-assets/before',
-			array( $this, 'register_editor_assets' )
+			array($this, 'register_editor_assets')
 		);
 	}
 
-	public function register_editor_assets() {
+	public function register_block_type()
+	{
+		if (!function_exists('register_block_type_from_metadata')) {
+			return;
+		}
+
+		register_block_type_from_metadata(
+			$this->get_dir('assets/src/editor/blocks/macros-inserter')
+		);
+	}
+
+	public function register_editor_assets()
+	{
 		// CodeMirror for wp.codeEditor.initialize(...)
 		wp_enqueue_code_editor(
 			array(
@@ -55,27 +76,27 @@ class Module implements
 		);
 
 		// Styles for CodeMirror in WP
-		wp_enqueue_style( 'wp-codemirror' );
+		wp_enqueue_style('wp-codemirror');
 
-		$asset_path = $this->get_dir( 'assets/build/editor.asset.php' );
+		$asset_path = $this->get_dir('assets/build/editor.asset.php');
 
-		$asset = file_exists( $asset_path )
+		$asset = file_exists($asset_path)
 			? require $asset_path
 			: array(
 				'dependencies' => array(),
-				'version'      => jet_form_builder()->get_version(),
+				'version' => jet_form_builder()->get_version(),
 			);
 
 		$deps = array_unique(
 			array_merge(
 				$asset['dependencies'] ?? array(),
-				array( 'code-editor' )
+				array('code-editor')
 			)
 		);
 
 		wp_enqueue_script(
-			$this->get_handle( 'editor' ),
-			$this->get_url( 'assets/build/editor.js' ),
+			$this->get_handle('editor'),
+			$this->get_url('assets/build/editor.js'),
 			$deps,
 			$asset['version'] ?? jet_form_builder()->get_version(),
 			true

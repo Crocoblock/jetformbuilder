78fa1db0346160cf15d2cff3634cc151fc2801e7
yurkodmtr <4au.4au.4au@gmail.com>
2026-04-21 17:32:19 +0300

    UPD: run buid script

--- modules/macros-inserter/assets/build/editor.asset.php
+++ modules/macros-inserter/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-element', 'wp-hooks'), 'version' => 'cf36b5e4fb7494570732');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-element', 'wp-hooks'), 'version' => 'b6ed2200064e4df58fd1');

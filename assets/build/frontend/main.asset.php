89cd6cabc84901b14ff23fb7af7dcac049246abd
ObitoTM <obito@templatemonster.me>
2026-05-01 17:32:14 +0300

    FIX: toDateTime default timezone handling https://github.com/Crocoblock/issues-tracker/issues/19565

--- assets/build/frontend/main.asset.php
+++ assets/build/frontend/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '5ac6f776029031c63469');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '9b3b8ea1753494067136');

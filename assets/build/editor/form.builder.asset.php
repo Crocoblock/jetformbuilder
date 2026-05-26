525b6a83bc308c0dad1544aa62fdbeff48062941
yurkodmtr <4au.4au.4au@gmail.com>
2026-05-26 14:31:12 +0300

    FIX: add fix for issue 19839

--- assets/build/editor/form.builder.asset.php
+++ assets/build/editor/form.builder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b23f3a9378f82ce10481');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '5a7617111d99de4504ef');

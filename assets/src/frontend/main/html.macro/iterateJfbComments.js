0cb307208e2d080f7b86089510f8b0fe1ead502c
ObitoTM <obito@templatemonster.me>
2026-06-11 18:38:47 +0300

    FIX: duplicate HTML macro comment processing https://github.com/Crocoblock/issues-tracker/issues/19073

--- assets/src/frontend/main/html.macro/iterateJfbComments.js
+++ assets/src/frontend/main/html.macro/iterateJfbComments.js
@@ -2,7 +2,7 @@ import iterateComments from './iterateComments';
 
 function* iterateJfbComments( rootNode ) {
 	const acceptCallback = node => {
-		return node.textContent.includes( 'JFB_FIELD::' );
+		return !node.jfbObserved && node.textContent.includes( 'JFB_FIELD::' );
 	};
 
 	yield* iterateComments( rootNode, acceptCallback );

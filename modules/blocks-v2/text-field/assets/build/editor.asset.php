68eaaea8ba8be05720005cbb92fd3bbe40929941
yurkodmtr <4au.4au.4au@gmail.com>
2026-07-07 10:37:07 +0300

    ADD: Customizable autocomplete attribute for text fields to improve accessibility https://github.com/Crocoblock/issues-tracker/issues/20129

--- modules/blocks-v2/text-field/assets/build/editor.asset.php
+++ modules/blocks-v2/text-field/assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '1b84345819dcebe8dab3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '1bd176f4ecf965034cf9');

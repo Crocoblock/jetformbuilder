95fd271871aa158be3f8df229c1f518d091d59c1
yurkodmtr <4au.4au.4au@gmail.com>
2026-07-20 10:25:18 +0300

    FIX: error appear in the console when add the field https://github.com/Crocoblock/issues-tracker/issues/19837

--- compatibility/elementor/elementor.php
+++ compatibility/elementor/elementor.php
@@ -17,7 +17,7 @@ use JFB_Compatibility\Elementor\Widgets;
 use JFB_Components\Module\Base_Module_Url_It;
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (! defined('WPINC')) {
 	die;
 }
 
@@ -26,7 +26,8 @@ class Elementor implements
 	Base_Module_Handle_It,
 	Base_Module_Url_It,
 	Base_Module_Dir_It,
-	Base_Module_After_Install_It {
+	Base_Module_After_Install_It
+{
 
 	use Base_Compat_Handle_Trait;
 	use Base_Compat_Url_Trait;
@@ -39,65 +40,70 @@ class Elementor implements
 	 */
 	private $onboarding_builder;
 
-	public function rep_item_id() {
+	public function rep_item_id()
+	{
 		return 'elementor';
 	}
 
-	public function condition(): bool {
-		return defined( 'ELEMENTOR_VERSION' );
+	public function condition(): bool
+	{
+		return defined('ELEMENTOR_VERSION');
 	}
 
-	public function on_install() {
+	public function on_install()
+	{
 		$this->onboarding_builder = new Onboarding_Builder();
 	}
 
-	public function on_uninstall() {
-	}
+	public function on_uninstall() {}
 
-	public function init_hooks() {
-		add_action( 'elementor/init', array( $this, 'init_widgets' ) );
-		add_action( 'elementor/editor/after_enqueue_styles', array( $this, 'editor_styles' ) );
-		add_action( 'elementor/preview/enqueue_scripts', array( $this, 'enqueue_form_scripts' ), 9 );
-		add_action( 'elementor/preview/enqueue_styles', array( $this, 'enqueue_form_styles' ) );
-		add_action( 'elementor/elements/categories_registered', array( $this, 'register_category' ) );
+	public function init_hooks()
+	{
+		add_action('elementor/init', array($this, 'init_widgets'));
+		add_action('elementor/editor/after_enqueue_styles', array($this, 'editor_styles'));
+		add_action('elementor/preview/enqueue_scripts', array($this, 'enqueue_form_scripts'), 9);
+		add_action('elementor/preview/enqueue_styles', array($this, 'enqueue_form_styles'));
+		add_action('elementor/elements/categories_registered', array($this, 'register_category'));
 
 		$this->get_onboarding_builder()->init_hooks();
 
 		// compatibility with 3.7
 		if (
-			defined( 'ELEMENTOR_VERSION' ) &&
-			version_compare( ELEMENTOR_VERSION, '3.5.0', '>=' )
+			defined('ELEMENTOR_VERSION') &&
+			version_compare(ELEMENTOR_VERSION, '3.5.0', '>=')
 		) {
-			add_action( 'elementor/widgets/register', array( $this, 'register_widgets' ) );
+			add_action('elementor/widgets/register', array($this, 'register_widgets'));
 		} else {
-			add_action( 'elementor/widgets/widgets_registered', array( $this, 'register_widgets' ) );
+			add_action('elementor/widgets/widgets_registered', array($this, 'register_widgets'));
 		}
 	}
 
-	public function remove_hooks() {
-		remove_action( 'elementor/init', array( $this, 'init_widgets' ) );
-		remove_action( 'elementor/editor/after_enqueue_styles', array( $this, 'editor_styles' ) );
-		remove_action( 'elementor/preview/enqueue_scripts', array( $this, 'enqueue_form_scripts' ), 9 );
-		remove_action( 'elementor/preview/enqueue_styles', array( $this, 'enqueue_form_styles' ) );
-		remove_action( 'elementor/elements/categories_registered', array( $this, 'register_category' ) );
+	public function remove_hooks()
+	{
+		remove_action('elementor/init', array($this, 'init_widgets'));
+		remove_action('elementor/editor/after_enqueue_styles', array($this, 'editor_styles'));
+		remove_action('elementor/preview/enqueue_scripts', array($this, 'enqueue_form_scripts'), 9);
+		remove_action('elementor/preview/enqueue_styles', array($this, 'enqueue_form_styles'));
+		remove_action('elementor/elements/categories_registered', array($this, 'register_category'));
 
 		// compatibility with 3.7
 		if (
-			defined( 'ELEMENTOR_VERSION' ) &&
-			version_compare( ELEMENTOR_VERSION, '3.5.0', '>=' )
+			defined('ELEMENTOR_VERSION') &&
+			version_compare(ELEMENTOR_VERSION, '3.5.0', '>=')
 		) {
-			remove_action( 'elementor/widgets/register', array( $this, 'register_widgets' ) );
+			remove_action('elementor/widgets/register', array($this, 'register_widgets'));
 		} else {
-			remove_action( 'elementor/widgets/widgets_registered', array( $this, 'register_widgets' ) );
+			remove_action('elementor/widgets/widgets_registered', array($this, 'register_widgets'));
 		}
 	}
 
-	public function init_widgets() {
+	public function init_widgets()
+	{
 		$this->types = array(
 			new Widgets\Form(),
 		);
 
-		foreach ( $this->types as $type ) {
+		foreach ($this->types as $type) {
 			$type->init_hooks();
 		}
 	}
@@ -107,14 +113,15 @@ class Elementor implements
 	 *
 	 * @return void
 	 */
-	public function register_category() {
+	public function register_category()
+	{
 
 		$elements_manager = \Elementor\Plugin::instance()->elements_manager;
 
 		$elements_manager->add_category(
 			'jet-form-builder',
 			array(
-				'title' => esc_html__( 'JetFormBuilder', 'jet-form-builder' ),
+				'title' => esc_html__('JetFormBuilder', 'jet-form-builder'),
 				'icon'  => 'font',
 			)
 		);
@@ -125,23 +132,24 @@ class Elementor implements
 	 *
 	 * @return void
 	 */
-	public function editor_styles() {
+	public function editor_styles()
+	{
 		wp_enqueue_style(
-			$this->get_handle( 'icons' ),
-			$this->get_url( 'assets/build/css/icons.css' ),
+			$this->get_handle('icons'),
+			$this->get_url('assets/build/css/icons.css'),
 			array(),
 			jet_form_builder()->get_version()
 		);
 	}
 
-
-	public function register_widgets( $manager ) {
-		foreach ( $this->types as $widget ) {
+	public function register_widgets($manager)
+	{
+		foreach ($this->types as $widget) {
 			// compatibility with 3.7
-			if ( method_exists( $manager, 'register' ) ) {
-				$manager->register( $widget );
+			if (method_exists($manager, 'register')) {
+				$manager->register($widget);
 			} else {
-				$manager->register_widget_type( $widget );
+				$manager->register_widget_type($widget);
 			}
 		}
 	}
@@ -149,27 +157,106 @@ class Elementor implements
 	/**
 	 * @noinspection PhpUnhandledExceptionInspection
 	 */
-	public function enqueue_form_scripts() {
+	public function enqueue_form_scripts()
+	{
 		/** @var Blocks\Module $blocks */
-		$blocks = jet_form_builder()->module( 'blocks' );
+		$blocks = jet_form_builder()->module('blocks');
+
 		/** @var Deprecated\Module $deprecated */
-		$deprecated = jet_form_builder()->module( 'deprecated' );
+		$deprecated = jet_form_builder()->module('deprecated');
 
 		$blocks->enqueue_frontend_assets();
 
+		// Elementor renders the selected form dynamically inside the preview.
+		// At this point, block view scripts enqueued while rendering the form
+		// are not automatically added to the already loaded preview document.
+		$this->enqueue_form_block_scripts();
+
+		// The WYSIWYG block requires WordPress editor scripts and settings.
+		// They must be loaded before the form is dynamically rendered.
+		wp_enqueue_editor();
+
 		// appointment/booking compatibility
 		$deprecated->register_scripts();
-		$deprecated->add_deprecated_script( '' );
+		$deprecated->add_deprecated_script('');
 	}
 
-	public function enqueue_form_styles() {
-		wp_enqueue_style( 'jet-form-builder-frontend' );
+	public function enqueue_form_styles()
+	{
+		wp_enqueue_style('jet-form-builder-frontend');
+
+		// Load frontend styles of all JetFormBuilder blocks in the
+		// Elementor preview before a form is selected dynamically.
+		$this->enqueue_form_block_styles();
+	}
+
+	/**
+	 * Enqueue frontend scripts of all registered JetFormBuilder blocks.
+	 *
+	 * Elementor can render or replace a form dynamically after the preview
+	 * document has already loaded. Therefore, block scripts must be available
+	 * before the form initialization is triggered.
+	 *
+	 * @return void
+	 */
+	private function enqueue_form_block_scripts()
+	{
+		$registered_blocks = \WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+		foreach ($registered_blocks as $block_type) {
+			if (0 !== strpos($block_type->name, 'jet-forms/')) {
+				continue;
+			}
+
+			$script_handles = array_merge(
+				$block_type->script_handles,
+				$block_type->view_script_handles
+			);
+
+			foreach (array_unique($script_handles) as $handle) {
+				if (! $handle) {
+					continue;
+				}
+
+				wp_enqueue_script($handle);
+			}
+		}
+	}
+
+	/**
+	 * Enqueue frontend styles of all registered JetFormBuilder blocks.
+	 *
+	 * @return void
+	 */
+	private function enqueue_form_block_styles()
+	{
+		$registered_blocks = \WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+		foreach ($registered_blocks as $block_type) {
+			if (0 !== strpos($block_type->name, 'jet-forms/')) {
+				continue;
+			}
+
+			$style_handles = array_merge(
+				$block_type->style_handles,
+				$block_type->view_style_handles
+			);
+
+			foreach (array_unique($style_handles) as $handle) {
+				if (! $handle) {
+					continue;
+				}
+
+				wp_enqueue_style($handle);
+			}
+		}
 	}
 
 	/**
 	 * @return Onboarding_Builder
 	 */
-	public function get_onboarding_builder(): Onboarding_Builder {
+	public function get_onboarding_builder(): Onboarding_Builder
+	{
 		return $this->onboarding_builder;
 	}
 }

3dd09f87a280815240a0c76cc1e8c32c6fb5612e
yurkodmtr <4au.4au.4au@gmail.com>
2026-07-20 10:46:56 +0300

    run build script

--- assets/build/editor/form.builder.asset.php
+++ assets/build/editor/form.builder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '62f366da7e196eddbd94');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b9ba9c5702d3989d5c56');

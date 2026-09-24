875af0df54b31d977692279e3172dd49824790a0
yurkodmtr <4au.4au.4au@gmail.com>
2026-09-24 11:00:47 +0300

    FIX: Input Mask doesn't work with Advanced Validation https://github.com/Crocoblock/issues-tracker/issues/15748

--- assets/build/frontend/advanced.reporting.asset.php
+++ assets/build/frontend/advanced.reporting.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch'), 'version' => 'fdceaad0341f9854c53f');
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '0aa0ea5fdf7870fbe31d');
--- assets/src/frontend/advanced.reporting/restrictions/MustEqual.js
+++ assets/src/frontend/advanced.reporting/restrictions/MustEqual.js
@@ -8,6 +8,24 @@ function MustEqual() {
 		return 'equal';
 	};
 	this.validate = function () {
+		if ( this.attrs.field ) {
+			const { input } = this.reporting;
+			const relatedInput = input.root.getInput( this.attrs.field );
+			const node = input.nodes[ 0 ];
+			const relatedNode = relatedInput?.nodes?.[ 0 ];
+
+			if ( node?.inputmask && relatedNode?.inputmask ) {
+				// Read both values directly: reactive values may temporarily be unmasked.
+				// node.value respects each field's autoUnmask option.
+				const value = node.inputmask.unmaskedvalue() ? node.value : '';
+				const expected = relatedNode.inputmask.unmaskedvalue()
+				                 ? relatedNode.value
+				                 : '';
+
+				return !value || value === expected;
+			}
+		}
+
 		const value = this.getValue();
 
 		return !value || value === this.attrs.value;
